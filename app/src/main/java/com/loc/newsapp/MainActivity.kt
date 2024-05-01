69972811fc1ba30c8d0a963ba295cd030009b718
Daemon Jung <jjjssswww1006@gmail.com>
2024-05-02 03:26:49 +0900

    Add NavGraph

--- app/src/main/java/com/loc/newsapp/MainActivity.kt
+++ app/src/main/java/com/loc/newsapp/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.material3.MaterialTheme
@@ -12,8 +13,10 @@ import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.WindowCompat
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.loc.newsapp.domain.manager.usecases.AppEntryUseCases
 import com.loc.newsapp.presentation.common.OnBoardingScreen
+import com.loc.newsapp.presentation.nvgraph.NavGraph
 import com.loc.newsapp.presentation.onboarding.OnBoardingViewModel
 import com.loc.newsapp.ui.theme.NewsAppTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -22,29 +25,30 @@ import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-    @Inject
-    lateinit var useCases: AppEntryUseCases
+//    @Inject
+//    lateinit var useCases: AppEntryUseCases
+    val viewModel by viewModels<MainViewModel>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         WindowCompat.setDecorFitsSystemWindows(window, false)
-        installSplashScreen()
-
-        lifecycleScope.launch {
-            useCases.readAppEntry().collect{
-                Log.d("Test", it.toString())
+        installSplashScreen().apply {
+            setKeepOnScreenCondition{
+                viewModel.splashCondition
             }
         }
+
+//        lifecycleScope.launch {
+//            useCases.readAppEntry().collect{
+//                Log.d("Test", it.toString())
+//            }
+//        }
 
         setContent {
             NewsAppTheme {
                 Box(modifier = Modifier.background(color = MaterialTheme.colorScheme.background)) {
-                    val viewModel: OnBoardingViewModel = hiltViewModel()
-                    OnBoardingScreen(
-                        event = viewModel::onEvent
-//                        event = {
-//                            viewModel.onEvent(it)
-//                        }
-                    )
+                
+                    val startDestination = viewModel.startDestination
+                    NavGraph(startDestination = startDestination)
                 }
             }
         }
---
+++ app/src/main/java/com/loc/newsapp/presentation/nvgraph/NavGraph.kt
@@ -0,0 +1,61 @@
+package com.loc.newsapp.presentation.nvgraph
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavGraph
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navigation
+import com.loc.newsapp.presentation.common.OnBoardingScreen
+import com.loc.newsapp.presentation.onboarding.OnBoardingViewModel
+
+@Composable
+fun NavGraph(
+    startDestination: String
+) {
+    val navController = rememberNavController()
+
+    NavHost(navController = navController, startDestination = startDestination){
+        navigation(
+            route = Route.AppStartNavigation.route,
+            startDestination = Route.OnBoardingScreen.route
+        ){
+            composable(
+                route = Route.OnBoardingScreen.route
+            ){
+                val viewModel: OnBoardingViewModel = hiltViewModel()
+                OnBoardingScreen(
+                    event = viewModel::onEvent
+                )
+            }
+        }
+
+        navigation(
+            route = Route.NewsNavigation.route,
+            startDestination = Route.HomeScreen.route
+        ){
+            composable(
+                route = Route.HomeScreen.route
+            ){
+                Text(text = "news")
+            }
+            composable(
+                route = Route.SearchScreen.route
+            ) {
+
+            }
+            composable(
+                route = Route.BookmarkScreen.route
+            ) {
+
+            }
+            composable(
+                route = Route.DetailsScreen.route
+            ) {
+
+            }
+        }
+    }
+}
