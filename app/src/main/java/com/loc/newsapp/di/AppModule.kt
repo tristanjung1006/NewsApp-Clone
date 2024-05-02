41aa2d235fa05cf35e8bd22149fc42c3a0cac815
Daemon Jung <jjjssswww1006@gmail.com>
2024-05-02 23:55:31 +0900

    change usecases directory

--- app/src/main/java/com/loc/newsapp/di/AppModule.kt
+++ app/src/main/java/com/loc/newsapp/di/AppModule.kt
@@ -3,9 +3,9 @@ package com.loc.newsapp.di
 import android.app.Application
 import com.loc.newsapp.data.manger.LocalUserMangerImpl
 import com.loc.newsapp.domain.manager.LocalUserManager
-import com.loc.newsapp.domain.manager.usecases.AppEntryUseCases
-import com.loc.newsapp.domain.manager.usecases.ReadAppEntry
-import com.loc.newsapp.domain.manager.usecases.SavedAppEntry
+import com.loc.newsapp.domain.usecases.app_entry.AppEntryUseCases
+import com.loc.newsapp.domain.usecases.app_entry.ReadAppEntry
+import com.loc.newsapp.domain.usecases.app_entry.SavedAppEntry
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
