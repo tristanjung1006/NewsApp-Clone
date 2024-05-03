d0bb336fa355f5f7609cc52a93860371837288a3
Daemon Jung <jjjssswww1006@gmail.com>
2024-05-03 16:45:21 +0900

    Add HomeViewModel

--- app/src/main/java/com/loc/newsapp/di/AppModule.kt
+++ app/src/main/java/com/loc/newsapp/di/AppModule.kt
@@ -2,14 +2,22 @@ package com.loc.newsapp.di
 
 import android.app.Application
 import com.loc.newsapp.data.manger.LocalUserMangerImpl
+import com.loc.newsapp.data.remote.NewsApi
+import com.loc.newsapp.data.repository.NewsRepositoryImpl
 import com.loc.newsapp.domain.manager.LocalUserManager
+import com.loc.newsapp.domain.repository.NewsRepository
 import com.loc.newsapp.domain.usecases.app_entry.AppEntryUseCases
 import com.loc.newsapp.domain.usecases.app_entry.ReadAppEntry
 import com.loc.newsapp.domain.usecases.app_entry.SavedAppEntry
+import com.loc.newsapp.domain.usecases.news.GetNews
+import com.loc.newsapp.domain.usecases.news.NewsUseCases
+import com.loc.newsapp.util.Constants.BASE_URL
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
 
 @Module
@@ -30,4 +38,30 @@ object AppModule {
         readAppEntry = ReadAppEntry(localUserManager),
         savedAppEntry = SavedAppEntry(localUserManager)
     )
+
+    @Provides
+    @Singleton
+    fun provideNewsApi(): NewsApi {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(NewsApi::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideNewsRepository(
+        newsApi: NewsApi
+    ): NewsRepository = NewsRepositoryImpl(newsApi)
+
+    @Provides
+    @Singleton
+    fun provideNewsUseCases(
+        newsRepository: NewsRepository
+    ): NewsUseCases {
+        return NewsUseCases(
+            getNews = GetNews(newsRepository)
+        )
+    }
 }
