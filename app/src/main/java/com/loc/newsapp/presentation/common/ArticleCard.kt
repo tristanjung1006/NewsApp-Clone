1256ac1b3d450c8dcf9ffee9097c0cf6b9286154
Daemon Jung <jjjssswww1006@gmail.com>
2024-05-08 21:31:48 +0900

    Add AsyncImage contentscale crop

--- app/src/main/java/com/loc/newsapp/presentation/common/ArticleCard.kt
+++ app/src/main/java/com/loc/newsapp/presentation/common/ArticleCard.kt
@@ -20,6 +20,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
@@ -54,7 +55,8 @@ fun ArticleCard(
                 .size(ArticleCardSize)
                 .clip(MaterialTheme.shapes.medium),
             model = ImageRequest.Builder(context).data(article.urlToImage).build(),
-            contentDescription = null)
+            contentDescription = null,
+            contentScale = ContentScale.Crop)
     }
 
     Column(
