b00ab79b40d3115e97d987fbcbc7951bf83c3291
Daemon Jung <jjjssswww1006@gmail.com>
2024-05-25 23:48:39 +0900

    chore

--- app/src/main/java/com/loc/newsapp/util/Constants.kt
+++ app/src/main/java/com/loc/newsapp/util/Constants.kt
@@ -7,4 +7,8 @@ object Constants {
     const val APP_ENTRY = "appEntry"
 
     const val API_KEY = ""
+
+    const val BASE_URL = "https://newsapi.org/v2/"
+
+    const val NEWS_DATABASE_NAME = "news_db"
 }
